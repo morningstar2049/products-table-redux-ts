2e5f07967491004e2e7bd2d493b6b09f1892c1aa
Vakho <vakho.daraselia@gmail.com>
2023-05-22 17:26:33 +0400

    added json-server and basic table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-import React from "react";
 import "./App.css";
-import { Table } from "@mui/material";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { useEffect } from "react";
+import { fetchProduct } from "./app/store";
 
 function App() {
+  const products = useAppSelector((state) => state.productReducer.products);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProduct());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <Table>something</Table>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>"id"</TableCell>
+            <TableCell>code</TableCell>
+            <TableCell>description</TableCell>
+            <TableCell>image</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {products.map((item) => (
+            <TableRow>
+              <TableCell>{item.id}</TableCell>
+              <TableCell>{item.description}</TableCell>
+              <TableCell>{item.product_code}</TableCell>
+              <TableCell>{item.imageURL}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,45 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {
+  configureStore,
+  createAsyncThunk,
+  createSlice,
+} from "@reduxjs/toolkit";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
+interface InitialStateInterface {
+  products: {
+    id: number;
+    product_code: string;
+    description: string;
+    imageURL: string;
+  }[];
+}
+
+export const fetchProduct = createAsyncThunk(
+  "products/fetch",
+  async (thunkApi) => {
+    const response = await fetch("http://localhost:3000/products", {
+      method: "GET",
+    });
+    const data = await response.json();
+    return data;
+  }
+);
+
+const initialState: InitialStateInterface = { products: [] };
+export const ProductSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchProduct.fulfilled, (state, action) => {
+      state.products = action.payload;
+    });
   },
 });
+export const store = configureStore({
+  reducer: { productReducer: ProductSlice.reducer },
+});
 
-export type AppDispatch = typeof store.dispatch;
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
